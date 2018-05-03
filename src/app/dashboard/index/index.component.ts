5ccce8cf4d59626806547c3aa4cd7447d482e03a
zhaowei <1512763623@qq.com>
2018-05-03 22:44:47 +0800

    dd

--- src/app/dashboard/index/index.component.ts
+++ src/app/dashboard/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../../common/restService/dashboard-service';
+import { NgxEchartsService } from 'ngx-echarts';
 
 @Component({
   selector: 'app-index',
@@ -7,8 +8,11 @@ import { DashboardService } from '../../common/restService/dashboard-service';
   styleUrls: ['./index.component.less']
 })
 export class IndexComponent implements OnInit {
+  list = [];
+  chartOption = {}
 
-  constructor(private dashboardService: DashboardService) { }
+  constructor(private dashboardService: DashboardService,
+    private ngxEchartsService: NgxEchartsService) { }
 
   ngOnInit() {
     this.getCostsList();
@@ -20,12 +24,177 @@ export class IndexComponent implements OnInit {
         params2: 'list'
       },
       data: {
-
+        startTime: (new Date().getTime()) - 1000 * 60 * 60 * 24,
+        endTime: new Date().getTime() + 1000 * 60 * 60 * 24,
+        name: ['ht']
       }
     })
       .then(response => {
         const rep = (response as any);
-        debugger
+        this.list = rep.data;
+        let xD = [];
+        let sD = [];
+        this.list.forEach(item => {
+          xD.push(item.time);
+          sD.push(item.value)
+        })
+
+        this.setChart(xD, sD);
+        //debugger
       });
+    setTimeout(() => {
+      this.getCostsList()
+    }, 10000)
+  }
+
+  setChart(xD, sD) {
+    var upColor = '#00da3c';
+    var upBorderColor = '#008F28';
+    var downColor = '#ec0000';
+    var downBorderColor = '#8A0000';
+
+    this.chartOption = {
+      title: {
+        text: 'HT',
+        left: 0
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross'
+        }
+      },
+      legend: {
+        data: ['分钟K线']
+      },
+      grid: {
+        left: '10%',
+        right: '10%',
+        bottom: '15%'
+      },
+      xAxis: {
+        type: 'category',
+        data: xD,
+        scale: true,
+        boundaryGap: false,
+        axisLine: { onZero: false },
+        splitLine: { show: false },
+        splitNumber: 20,
+        min: 'dataMin',
+        max: 'dataMax'
+      },
+      yAxis: {
+        scale: true,
+        splitArea: {
+          show: true
+        }
+      },
+      dataZoom: [
+        {
+          type: 'inside',
+          start: 50,
+          end: 100
+        },
+        {
+          show: true,
+          type: 'slider',
+          y: '90%',
+          start: 50,
+          end: 100
+        }
+      ],
+      series: [
+        {
+          name: '日K',
+          type: 'candlestick',
+          data: sD,
+          itemStyle: {
+            normal: {
+              color: upColor,
+              color0: downColor,
+              borderColor: upBorderColor,
+              borderColor0: downBorderColor
+            }
+          },
+          markPoint: {
+            label: {
+              normal: {
+                formatter: function (param) {
+                  return param != null ? Math.round(param.value) : '';
+                }
+              }
+            },
+            data: [
+              {
+                name: 'XX标点',
+                coord: ['2013/5/31', 2300],
+                value: 2300,
+                itemStyle: {
+                  normal: { color: 'rgb(41,60,85)' }
+                }
+              },
+              {
+                name: 'highest value',
+                type: 'max',
+                valueDim: 'highest'
+              },
+              {
+                name: 'lowest value',
+                type: 'min',
+                valueDim: 'lowest'
+              },
+              {
+                name: 'average value on close',
+                type: 'average',
+                valueDim: 'close'
+              }
+            ],
+            tooltip: {
+              formatter: function (param) {
+                return param.name + '<br>' + (param.data.coord || '');
+              }
+            }
+          },
+          markLine: {
+            symbol: ['none', 'none'],
+            data: [
+              [
+                {
+                  name: 'from lowest to highest',
+                  type: 'min',
+                  valueDim: 'lowest',
+                  symbol: 'circle',
+                  symbolSize: 10,
+                  label: {
+                    normal: { show: false },
+                    emphasis: { show: false }
+                  }
+                },
+                {
+                  type: 'max',
+                  valueDim: 'highest',
+                  symbol: 'circle',
+                  symbolSize: 10,
+                  label: {
+                    normal: { show: false },
+                    emphasis: { show: false }
+                  }
+                }
+              ],
+              {
+                name: 'min line on close',
+                type: 'min',
+                valueDim: 'close'
+              },
+              {
+                name: 'max line on close',
+                type: 'max',
+                valueDim: 'close'
+              }
+            ]
+          }
+        }
+      ]
+    };
   }
 }
